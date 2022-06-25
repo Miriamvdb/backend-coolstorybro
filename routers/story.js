b0b2665aa92cdd32bc4a16651831d61b5d5c52a6
Miriamvdb <miriamvdb@live.nl>
2022-06-25 20:43:23 +0200

    Backend/Feature4: router delete story

--- routers/story.js
+++ routers/story.js
@@ -5,18 +5,34 @@ const Story = require("../models").story;
 // instantiate a router
 const router = new Router();
 
-// register a GET / route that makes a test
 // TEST: http :4000/stories/testestest
 router.get("/testestest", (req, res) => {
   res.send("Testestest..");
 });
 
-// GET all stories `localhost:4000/`
 // http GET :4000/stories/
 router.get("/", async (req, res) => {
   try {
     const allStories = await Story.findAll();
     res.send(allStories);
+  } catch (e) {
+    console.log(e.message);
+  }
+});
+
+// http DELETE :4000/stories/1
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const storyToDelete = await Story.findByPk(id);
+
+    if (!storyToDelete) {
+      console.log("Story not found!");
+    }
+
+    await storyToDelete.destroy();
+
+    return res.send("Story deleted");
   } catch (e) {
     console.log(e.message);
   }
